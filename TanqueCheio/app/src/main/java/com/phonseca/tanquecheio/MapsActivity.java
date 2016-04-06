251bd329784d6aea15f1566542c69fb798eaaa10
phonseca <pablo.h.fonseca@gmail.com>
2016-04-06 16:27:28 -0300

    TanqueCheio - Ponto mais proximo
    Criada logica para itendificar ponto mais próximo a localizacao atual,
    considerando uma linha reta.

--- TanqueCheio/app/src/main/java/com/phonseca/tanquecheio/MapsActivity.java
+++ TanqueCheio/app/src/main/java/com/phonseca/tanquecheio/MapsActivity.java
@@ -22,6 +22,8 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.phonseca.tanquecheio.helpers.JsonHelper;
 import com.phonseca.tanquecheio.models.PostoCombustivel;
 
@@ -64,7 +66,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
 
         setupPostosCombustivel();
-        obterLocalizacaoAtual(location);
+
+        LatLng localizacaoAtual = obterLocalizacaoAtual(location);
+        LatLng localizacaoPostoMaisProximo = obterLocalizacaoPostoMaisProximo(localizacaoAtual);
+
+        desenharLinha(localizacaoAtual, localizacaoPostoMaisProximo);
     }
 
 
@@ -79,7 +85,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private String leJsonPostosCombustivel() {
-        String json = "[{\"nome\":\"Teste\",\"latitude\":-19.916667,\"longitude\":-43.933333,\"precoGasolina\": 3.89}]";
+        String json = "[{\"nome\":\"Posto1\",\"latitude\":-21.7543446,\"longitude\":-43.350265,\"precoGasolina\": 3.89}," +
+                "{\"nome\":\"Posto2\",\"latitude\":-21.7670446,\"longitude\":-43.329265,\"precoGasolina\": 3.78}," +
+                "{\"nome\":\"Posto3\",\"latitude\":-21.7679446,\"longitude\":-43.351065,\"precoGasolina\": 3.92}]";
 
         return json;
     }
@@ -93,19 +101,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             for (PostoCombustivel posto : listaPostos) {
 
-                Marker marker = mMap.addMarker(new MarkerOptions()
+                mMap.addMarker(new MarkerOptions()
                         .position(new LatLng(posto.getLatitude(), posto.getLongitude()))
                         .title(posto.getNome())
                         .snippet(String.valueOf(posto.getPrecoGasolina()))
                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(posto.getLatitude(), posto.getLongitude()), 8));
             }
-
         }
     }
 
-    private void obterLocalizacaoAtual(Location location) {
+    private LatLng obterLocalizacaoAtual(Location location) {
         if (location != null) {
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
                     new LatLng(location.getLatitude(), location.getLongitude()), 13));
@@ -118,26 +123,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .build();                   // Creates a CameraPosition from the builder
             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
-        }else
-        {
+        } else {
             location = new Location("");
 
             location.setLatitude(-21.7672446);
-            location.setAltitude(-43.350865);
+            location.setLongitude(-43.350865);
 
+            mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                    .title("Você está aqui!"));
 
-
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                    new LatLng(location.getLatitude(), location.getLongitude()), 13));
-
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
-                    .zoom(17)                   // Sets the zoom
-                    .bearing(90)                // Sets the orientation of the camera to east
-                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
-                    .build();                   // Creates a CameraPosition from the builder
-            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 14));
         }
+
+        return new LatLng(location.getLatitude(), location.getLongitude());
+    }
+
+    private LatLng obterLocalizacaoPostoMaisProximo(LatLng pontoAtual) {
+
+        double distancia = 0;
+        LatLng localizacaoPostoMaisProximo = null;
+
+        Location localAtual = new Location("");
+        localAtual.setLatitude(pontoAtual.latitude);
+        localAtual.setLongitude(pontoAtual.longitude);
+
+        for(PostoCombustivel posto : listaPostos)
+        {
+            Location localPosto = new Location("");
+            localPosto.setLatitude(posto.getLatitude());
+            localPosto.setLongitude(posto.getLongitude());
+
+            double distanciaAtual = localAtual.distanceTo(localPosto);
+
+            if (distancia == 0 || distanciaAtual < distancia )
+            {
+                distancia = distanciaAtual;
+                localizacaoPostoMaisProximo = new LatLng(posto.getLatitude(), posto.getLongitude());
+            }
+
+        }
+
+        return localizacaoPostoMaisProximo;
+    }
+
+    private void desenharLinha(LatLng posicaoAtual, LatLng posicaoDestino) {
+        PolylineOptions rectOptions = new PolylineOptions()
+                .add(posicaoAtual)
+                .add(posicaoDestino);
+
+        Polyline polyline = mMap.addPolyline(rectOptions);
+        polyline.setWidth(5);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posicaoAtual, 14));
+
     }
 
 }
